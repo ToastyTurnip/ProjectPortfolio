6460d1fcddd2affcec3cefafaf24c849bbbf7dc9
jbatoast <112858093+jbatoast@users.noreply.github.com>
2024-11-13 04:52:32 +0800

    Changed icon, added working links

--- src/lib/portfoliocards.svelte
+++ src/lib/portfoliocards.svelte
@@ -4,12 +4,12 @@
             <h2
                 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white"
             >
-                🚧 Projects (WIP) 🚧
+                Projects
             </h2>
             <p
                 class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400"
             >
-                Portfolio coming soon!
+                View some of my projects!
             </p>
         </div>
 
@@ -31,7 +31,7 @@
                     A Web applciation developed using SvelteKit, PostgreSQL, and Flask used by the Faculty and Students for managing departmental resources.
                 </p>
                 <a
-                    href="#"
+                    href="http://209.135.170.7:4000"
                     title=""
                     class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     role="button"
@@ -68,12 +68,49 @@
                     Personal discord bot used for managing home automation and other utility functions.
                 </p>
                 <a
-                    href="#"
+                    href="https://discord.com/oauth2/authorize?client_id=786515365639225394&permissions=8&integration_type=0&scope=bot ignore dis"
                     title=""
                     class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     role="button"
                 >
                     Invite to your server
+                    <svg
+                        aria-hidden="true"
+                        class="w-5 h-5 ml-2 -mr-1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </a>
+            </div>
+
+            <div class="space-y-4">
+                <span
+                    class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
+                >
+                    Website
+                </span>
+                <h3
+                    class="text-2xl font-bold leading-tight text-gray-900 dark:text-white"
+                >
+                    PAWtection
+                </h3>
+                <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+                    A website mockup for insurance services created as a supplement to another unrelated project.
+                </p>
+                <a
+                    href="https://toastyturnip.github.io/pawtection/"
+                    title=""
+                    class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    role="button"
+                >
+                    View website
                     <svg
                         aria-hidden="true"
                         class="w-5 h-5 ml-2 -mr-1"
@@ -105,7 +142,7 @@
                     Understanding fake news proliferation through data analysis using Python and basic Natural Language Processing. 
                 </p>
                 <a
-                    href="#"
+                    href="https://jbatoast.github.io/Group-57/"
                     title=""
                     class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     role="button"
@@ -147,7 +184,7 @@
                     class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     role="button"
                 >
-                    View project
+                    View project (TBA)
                     <svg
                         aria-hidden="true"
                         class="w-5 h-5 ml-2 -mr-1"
